9841f7494bf6a41359cc35deb154ed94230be8a6
Ricardo Alexandre de Souza Ribeiro <ralexandre11@gmail.com>
2021-01-02 12:18:19 -0300

    Map entities

--- src/main/java/com/springapi/domain/Request.java
+++ src/main/java/com/springapi/domain/Request.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.springapi.domain.enums.RequestState;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +28,32 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "request")
 public class Request {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name = "creation_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date creationDate;
-	private RequestStage state;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@OneToMany(mappedBy = "request")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
