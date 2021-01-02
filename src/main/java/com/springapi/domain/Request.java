66533fee25166a4811f4fbc8013df1afb928fc76
Ricardo Alexandre de Souza Ribeiro <ralexandre11@gmail.com>
2021-01-02 15:01:31 -0300

    Config Serialization

--- src/main/java/com/springapi/domain/Request.java
+++ src/main/java/com/springapi/domain/Request.java
@@ -1,5 +1,6 @@
 package com.springapi.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,8 +30,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "request")
-public class Request {
+public class Request implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
--- src/main/java/com/springapi/domain/User.java
+++ src/main/java/com/springapi/domain/User.java
@@ -1,5 +1,6 @@
 package com.springapi.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,8 +25,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "user")
-public class User {
+public class User implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
