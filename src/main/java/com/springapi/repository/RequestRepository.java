850cc956b22cc2eea49e1a3372555ed12ba094c3
Ricardo Alexandre de Souza Ribeiro <ralexandre11@gmail.com>
2021-01-07 12:27:34 -0300

    Tests

--- src/main/java/com/springapi/repository/RequestRepository.java
+++ src/main/java/com/springapi/repository/RequestRepository.java
@@ -2,9 +2,12 @@ package com.springapi.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springapi.domain.Request;
 import com.springapi.domain.enums.RequestState;
@@ -14,6 +17,9 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 	
 	public List<Request> findAllByOwnerId(Long id);
 	
+	
+	@Transactional(readOnly = false)
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
 	public Request updateStatus(Long id, RequestState state);
 
---
+++ src/test/java/com/springapi/respository/ResquestStageRepositoryTests.java
@@ -0,0 +1,5 @@
+package com.springapi.respository;
+
+public class ResquestStageRepositoryTests {
+
+}
